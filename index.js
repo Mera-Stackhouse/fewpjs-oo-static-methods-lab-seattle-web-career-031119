010d41f80748abe3d9dfc903e2e8c3dc1895a2d7
Mera-Stackhouse <mera.stackhouse@gmail.com>
2019-05-02 15:57:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string = string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(string) {
+    string = string.charAt(0).toUpperCase() + string.slice(1)
+    const array = string.split(" ")
+    const newArray = []
+    for (const item of array) {
+      if (['the', 'a', 'an', 'but', 'my', 'of', 'and', 'for', 'is', 'at', 'by', 'from'].includes(item)) {
+        newArray.push(item)
+      } else {
+        const newItem = item.charAt(0).toUpperCase() + item.slice(1)
+        newArray.push(newItem)
+      }
+    }
+    return newArray.join(" ")
+  }
+
 }
